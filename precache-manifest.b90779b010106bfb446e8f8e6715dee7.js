980ddfc1a108657cba26ac59bb622735c0605109
Jakob <61665831+jakobloke@users.noreply.github.com>
2020-07-26 21:54:07 -0700

    Updates

---
+++ precache-manifest.b90779b010106bfb446e8f8e6715dee7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4674dd71d3c370fc0d3de278abbb5438",
+    "url": "/jakob-portfolio/index.html"
+  },
+  {
+    "revision": "e53d4f19598e541613c7",
+    "url": "/jakob-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d82d29e1cc5b1f7eceac",
+    "url": "/jakob-portfolio/static/css/main.89ae68a0.chunk.css"
+  },
+  {
+    "revision": "e53d4f19598e541613c7",
+    "url": "/jakob-portfolio/static/js/2.9f4b65fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jakob-portfolio/static/js/2.9f4b65fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82d29e1cc5b1f7eceac",
+    "url": "/jakob-portfolio/static/js/main.4243f767.chunk.js"
+  },
+  {
+    "revision": "7cd3bba903f57ef2d3b2",
+    "url": "/jakob-portfolio/static/js/runtime-main.8a825421.js"
+  },
+  {
+    "revision": "992a5ee2892e3848904af32ecd54c12a",
+    "url": "/jakob-portfolio/static/media/cover.992a5ee2.JPEG"
+  },
+  {
+    "revision": "7dbed3de46b40b99789af83a9b2bcc40",
+    "url": "/jakob-portfolio/static/media/jakob.7dbed3de.JPG"
+  },
+  {
+    "revision": "1afd5ac60e66d36954a8fb953136c3c7",
+    "url": "/jakob-portfolio/static/media/linkedin.1afd5ac6.png"
+  }
+]);
